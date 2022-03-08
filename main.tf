52e8a4e7c5f46bddca5b8b374be77a6db864ad86
Kevin Jakubczak <Kevin.M.Jakubczak@sherwin.com>
2022-03-08 15:28:19 +0000

    Update.

--- main.tf
+++ main.tf
@@ -17,4 +17,17 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "corp-datamiddlwre-platopt-kmj251-sb-001-rg"
   location = "eastus2"
+  tags = {
+    "CostCenter"   = "D100-84IM24"
+    "DMZ"          = "No" 
+    "DeletionDate" = "12/31/2022" 
+    "Department"   = "Data and Middleware" 
+    "Division"     = "Corp" 
+    "Environment"  = "Sandbox" 
+    "ManagedBy"    = "Ansible" 
+    "Owner"        = "Kevin Jakubczak | kevin.m.jakubczak@sherwin.com" 
+    "Program"      = "Item Management" 
+    "Project"      = "PPM 220086" 
+    "SubnetID"     = "274" 
+  }
 }
