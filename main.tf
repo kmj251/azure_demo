7c52c44d614e9b26cfe019a43dfe4a237c8231e2
Kevin Jakubczak <Kevin.M.Jakubczak@sherwin.com>
2022-03-08 15:57:01 +0000

    Update.

--- main.tf
+++ main.tf
@@ -15,19 +15,39 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "corp-datamiddlwre-platopt-kmj251-sb-001-rg"
+  name     = var.resource_group_name
   location = "eastus2"
   tags = {
-    "CostCenter"   = "D100-84IM24"
-    "DMZ"          = "No" 
-    "DeletionDate" = "12/31/2022" 
-    "Department"   = "Data and Middleware" 
-    "Division"     = "Corp" 
-    "Environment"  = "Sandbox" 
-    "ManagedBy"    = "Ansible" 
-    "Owner"        = "Kevin Jakubczak | kevin.m.jakubczak@sherwin.com" 
-    "Program"      = "Item Management" 
-    "Project"      = "PPM 220086" 
-    "SubnetID"     = "274" 
+    "CostCenter"   = "D100-84IM64"
+    "DMZ"          = "No"
+    "DeletionDate" = "12/31/2023"
+    "Department"   = "Cloud Platform"
+    "Division"     = "Corp"
+    "Environment"  = "Sandbox"
+    "ManagedBy"    = "Ansible"
+    "Owner"        = "Kevin Jakubczak | kevin.m.jakubczak@sherwin.com"
+    "Program"      = "Cloud Platform"
+    "Project"      = "PPM 220086"
+    "SubnetID"     = "274"
   }
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "corp-datamiddlwre-platopt-kmj251-sb-001-rg-vnet"
+  address_space       = ["10.156.201.208/28"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    "CostCenter"  = "D100-84IM64"
+    "DMZ"         = "No"
+    "Department"  = "Cloud Platform"
+    "Environment" = "Sandbox"
+    "ManagedBy"   = "Ansible"
+    "Owner"       = "Kevin Jakubczak | kevin.m.jakubczak@sherwin.com"
+    "Program"     = "Item Management"
+    "Project"     = "PPM 220086"
+    "Division"     = "Corp"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "resource_group_name" {
+  default = "corp-datamiddlwre-platopt-kmj251-sb-001-rg"
+}
